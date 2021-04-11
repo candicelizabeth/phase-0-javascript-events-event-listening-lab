295056a95d6d07b9af863596e1f882591b20c5a9
Candice Peters <candice.peters@flatironschool.com>
2021-04-11 17:58:21 -0500

    commit tree 6a68add100c31ea4bc1a7f5face58d6f0be908c1

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+}); 
 }
+//WALKTHROUGH LAB POINT OUT STUFF IN READ ME
